b2f2978e7557d71c3f093e72e926fec170566fa2
NoFatherToMyStyle <59022278+NoFatherToMyStyle@users.noreply.github.com>
2022-07-19 17:37:46 -0500

    Create BankAccount.cpp

---
+++ BankAccount/BankAccount.cpp
@@ -0,0 +1,54 @@
+#include "Account.h"
+
+BankAccount::BankAccount(std::string const owner)
+{
+	this->owner = owner;
+	balance = 0;
+}
+
+BankAccount::BankAccount(std::string const owner, int balance)
+{
+	this->owner = owner;
+	this->balance = balance;
+}
+
+void BankAccount::deposit(int amount)
+{
+	if (amount > 0)
+	{
+		balance += amount;
+	}
+	else
+	{
+		std::cout << "You must add a dollar amount greater than $0." << std::endl;
+	}
+}
+
+void BankAccount::withdraw(int amount)
+{
+	if (amount > 0)
+	{
+		if (balance > amount)
+		{
+			balance -= amount;
+		}
+		else
+		{
+			std::cout << "You don't have that much money." << std::endl;
+		}
+	}
+	else
+	{
+		std::cout<< "You must add a dollar amount greater than $0." << std::endl;
+	}
+}
+
+std::string BankAccount::getOwner() const
+{
+	return owner;
+}
+
+int BankAccount::getBalance() const
+{
+	return balance;
+}
